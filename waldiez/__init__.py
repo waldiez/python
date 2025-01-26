77de32b1a2c7494ad23a6441d390453f53b48009
Lazaros Toumanidis <laztoum@protonmail.com>
2025-01-26 02:51:32 +0200

    update __init__

--- waldiez/__init__.py
+++ waldiez/__init__.py
@@ -6,14 +6,20 @@ from ._version import __version__
 from .exporter import WaldiezExporter
 from .models import Waldiez
 from .runner import WaldiezRunner
-from .utils import check_conflicts, check_flaml_warnings, check_pysqlite3
+from .utils import check_conflicts, check_flaml_warnings
 
 __WALDIEZ_INITIALIZED = False
 
 if not __WALDIEZ_INITIALIZED:
     check_conflicts()
     check_flaml_warnings()
-    check_pysqlite3()
+    # let's skip the one below
+    # check_pysqlite3()
+    # and use it only if needed:
+    #   captain_agent dependency:
+    #   before calling pip install pyautogen[captainagent]
+    #   we should have pysqlite3 installed (at least on windows)
+    # before running a flow
     __WALDIEZ_INITIALIZED = True
 
 __all__ = [
