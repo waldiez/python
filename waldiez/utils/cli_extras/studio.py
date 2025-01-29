281b1246c384ca58bd5bafd931741c02af342606
Lazaros Toumanidis <laztoum@protonmail.com>
2025-01-29 22:01:19 +0200

    update cli_extras/studio.py

--- waldiez/utils/cli_extras/studio.py
+++ waldiez/utils/cli_extras/studio.py
@@ -13,7 +13,7 @@ HAVE_STUDIO = False
 studio_app: Callable[..., Any] | None = None
 
 try:
-    from waldiez_studio.cli import run  # type: ignore[import-untyped, unused-ignore, import-not-found]  # noqa
+    from waldiez_studio.cli import run  # type: ignore[unused-ignore, import-untyped]  # noqa  # flake8: noqa
 
     studio_app = run
 
