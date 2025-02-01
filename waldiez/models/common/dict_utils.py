28999fc2fde355e27fbc3fd1aa611bbe39402d23
Lazaros Toumanidis <laztoum@protonmail.com>
2025-02-01 12:29:33 +0200

    add missing headers

--- waldiez/models/common/dict_utils.py
+++ waldiez/models/common/dict_utils.py
@@ -1,5 +1,7 @@
-"""Dictionary related utilities."""
+# SPDX-License-Identifier: Apache-2.0.
+# Copyright (c) 2024 - 2025 Waldiez and contributors.
 
+"""Dictionary related utilities."""
 from typing import Any, Dict
 
 
