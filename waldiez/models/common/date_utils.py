4e80ca374178d99bf9b9e7453dfc9bb67c89dc78
Lazaros Toumanidis <laztoum@protonmail.com>
2025-02-01 12:33:35 +0200

    lint

--- waldiez/models/common/date_utils.py
+++ waldiez/models/common/date_utils.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0.
 # Copyright (c) 2024 - 2025 Waldiez and contributors.
-
 """Date utilities."""
+
 from datetime import datetime, timezone
 
 
--- waldiez/models/common/dict_utils.py
+++ waldiez/models/common/dict_utils.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0.
 # Copyright (c) 2024 - 2025 Waldiez and contributors.
-
 """Dictionary related utilities."""
+
 from typing import Any, Dict
 
 
